4c3c7f6717d5dae739af781b05994b4b67d99d3f
Brijithk <keranbrijith@gmail.com>
2026-04-15 17:24:58 +0530

    initial commit

---
+++ Demo.py
@@ -0,0 +1,144 @@
+# message="hello world"
+# print(message)
+# num=num1=num2=200
+# print(num)
+# print(num1)
+# name="Brijith keran"
+# Roll_no=1
+# semester=1
+# dept="mech"
+# phone=9448845675
+# email="keran@gmail.com"
+# city="kanyakumari"
+# address="kaithatu konam,puthenkadai,Thiruvattar"
+# name="Brijith keran"
+# print("Details :\n","name :",name,"Roll_no :",Roll_no,semester,dept,phone,email,city,address)
+
+#Conversion
+# int_eg=100
+# float_eg=3.14
+# print(int(float_eg))
+# print(float(int_eg))
+
+#you have a integer 10 and string "20".Covert the string into an integer and print their sum
+
+# int_a=10
+# str_b="20"
+# print("sum : ",int_a+int(str_b))
+
+# Arithmetic Operations in python
+# a=20
+# b=24
+# print("Addition :",a+b)
+# print("Subtraction :",a-b)
+# print("Division :",a/b)
+# print("Multiplication :",a*b)
+# print("Floor division",a//b)
+# print("Modulus",a%b)
+# print("Exponetiation",a**b)
+
+# Create a simple program to to solve the problem (a+b)^2
+
+# a=10
+# b=5
+# c=(a**a)+(b**2)+(2*a*b)
+# print(c)
+
+
+# name="abraham lincoln"
+#converts the firstletter of each word to uppercase
+# print(name.title())
+# #converts the string into uppercase
+# print(name.upper())
+# #converts the string into lowercase
+# print(name.lower())
+#to add a tab to your text
+# print("Apple\tOrange")
+#to add new line
+# print("Apple\nOrange")
+#rstrip()- removes white spaces from right end
+#lstrip()- removes white spaces from right end
+#strip()- removes white spaces from both end
+# name="  Faith  "
+# print(name.rstrip())
+# print(name.lstrip())
+# print(name.strip())
+
+#Declare a variable price "299.99", and convert it into float and print
+
+# price="299.99"
+# print(float(price))
+
+#convert total seconds into minute and seconds.(125 sec)
+
+# totalSeconds=125
+# minutes=0
+# seconds=0
+# while(totalSeconds>=60):
+#     minutes=minutes+1
+#     totalSeconds=totalSeconds-60
+# seconds=totalSeconds
+# print("the time is : ",minutes ," minutes",seconds," seconds" )
+
+#convert a float value 45.8 into an integer and print the result
+
+# value=45.8
+# print(int(value))
+
+#calculate the area of circle
+# r=5
+# pi=3.14
+# area=pi*r*r
+# print(area)
+
+#calculate the simple intrest (assignvalues for p,t,r)
+# p=1000
+# t=3
+# r=4
+# print("Simple Interest",p*t*(r/100))
+
+#create a variable temperature .convert it into farenheit by using f=(c*9/5)+32
+# temperature=90
+# f=(temperature*9/5)+32
+# print("the farenteit value is : ",f)
+
+#create two variables and print all the arithmetic operations
+# a=5
+# b=6
+# print("addition :",a+b)
+# print("subtraction :",a-b)
+# print("division :",a/b)
+# print("multiplication :",a*b)
+# print("Floor division",a//b)
+# print("Modulus",a%b)
+# print("Exponentiation",a**b)
+
+#string slicing
+# greet="Hello world"
+# print(greet[0])
+# print(greet[9])
+# print(greet[-3])
+# print(greet[2:5])
+# print(greet[3:])
+# print(greet[:5])
+# print(greet*2)
+# print(greet + "how are you")
+
+message="python is fun"
+#print the entire string stored in the variable message
+print(message)
+#print the second character of the string
+print(message[1])
+#print the first 6 characters of the string
+print(message[0:6])
+# print all the charcters starting from 8 till the end
+print(message[7:])
+# print the string 8 times
+print(message*8)
+# concatenate the message "lets learn" to the message
+newMsg="lets learn "+message
+print(newMsg)
+# print characters from index 3 to 8
+print(message[3:9])
+
+# vishnukala.m@faithinfotech.in
