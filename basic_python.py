a1c826116e42f4ddc66ec7f734d597feb948c76f
Brijithk <keranbrijith@gmail.com>
2026-04-16 12:09:09 +0530

    added concatenation

---
+++ basic_python.py
@@ -0,0 +1,14 @@
+# mystring='A single line string'
+# mystring2="This is a single line string"
+#for multiline strings use triple quotes
+#also called docstring
+#it will preserve the single quote as it is
+# mystring3='''This  is a multiline
+# string'''
+# print(mystring3)
+
+#string formatting used to insert variables inside a string
+#Mathod 1- using concatenation (error prone and not standard way)
+country_name="India"
+print(country_name+" is my country")
+print("All "+country_name+"n's are my brothers and sisters")
