71f6aaa9585c64281faddbb822667162e9a59137
Brijithk <keranbrijith@gmail.com>
2026-04-20 12:12:35 +0530

    added brak statement

--- condition.py
+++ condition.py
@@ -41,5 +41,35 @@ match dob_month:
     case 3:
         print("your stone is Aquamarine")
         print("People born in mar are naughty")
+    case 4:
+        print("your stone is Diamond")
+        print("People born in mar are caring")
     case _:
         print("invalid month entered")
+      
+
+#for loop example -  used to iterate through an iterable (list)
+fruits=['apple','orrange','banana','cherry']
+for index,each_fruit in enumerate(fruits):
+    #to display the index numbers use enumerate function
+    print(index+1,each_fruit)
+
+#to generate numerical iterbales (list of nos) use range()
+#range(start(optional),end,stepSizeOptional)
+for each_num in range(1,100,5):
+    print(each_num)
+
+#while loop- check condition and then execute the code
+counter_max=5
+while counter_max>0:
+    print("counter : ",counter_max)
+    counter_max-=1
+
+#break - to break the entire loop when a condition is met
+j=0
+for i in range(5):
+    j=j+2
+    print("i= ",i ,"j =", j)
+    if j==6:
+        continue
+    print("j value is",j)
